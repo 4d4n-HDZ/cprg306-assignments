1997ed7d5550cc0b0c7858f38c40fa1c63da4d4d
Adan HZ <153233028+4d4n-HDZ@users.noreply.github.com>
2026-01-23 22:20:04 -0700

    Week 1, Week 2, Week 3
    Pending files

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function StudentInfo() {
+  return (
+    <div>
+      <h1>Adan Hernandez</h1>
+      <Link href="https://github.com/4d4n-HDZ/cprg306-assignments">Click this to access my Github Repository</Link>
+    </div>
+  );
+}
+
+export default StudentInfo;
+
+
---
+++ app/week-2/page.js
@@ -0,0 +1,12 @@
+import StudentInfo from "./StudentInfo";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>My Student Information</h1>
+      < StudentInfo />
+    </main>
+
+  );
+}
+
